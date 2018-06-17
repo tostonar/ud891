750ea7fbaf1faf29fdd0c52c1ec7f75b15b8a29c
Andrea Toston <tostonar@gmail.com>
2018-06-17 14:12:47 -0400

    up to aria checkbox

--- lesson2-focus/03-managing-focus/scripts/main.js
+++ lesson2-focus/03-managing-focus/scripts/main.js
@@ -19,6 +19,15 @@ page('/:slug', function(context) {
   newMenuItem.classList.add('is-active');
   newPage.classList.add('is-active');
 
+  // If this is the first time someone is visiting the site, don't move focus around. Wait until they have clicked a menu item
+  if (isFirstPage) {
+    isFirstPage = false;
+    return;
+  }
+
+  // Move focus to a heading in the new page
+  newPage.querySelector('h2').focus();
+
 });
 
 page({
